0156f0158a2b7c5a0524c65e0195a880fe387338
JishT <45015391+JishT@users.noreply.github.com>
2018-12-18 13:25:53 -0500

    this is the fourth

--- src/Bullet.java
+++ src/Bullet.java
@@ -3,42 +3,44 @@ import java.awt.*;
 //https://stackoverflow.com/questions/44138641/2d-bullet-game-in-java
 
 
-public class Bullet
-{
-    int x,y, xM, yM;
-    private static final double speed = 2;
-    double dirX, dirY;
+public class Bullet {
+    int  xM, yM;
+    private static final double speed = 1.5;//has to be greater than 1
+    double x, y, dirX, dirY;
 
-    public Bullet(int x, int y, int xM, int yM)
-    {
-        this.x=x;
-        this.y=y;
+    public Bullet(int x, int y, int xM, int yM) {
+        this.x = x;
+        this.y = y;
         this.xM = xM;
         this.yM = yM;
 
-        dirX = speed*Math.cos(getAngle());
-        dirY = speed*Math.sin(getAngle());
+        dirX = speed * Math.cos(getAngle());
+        dirY = speed * Math.sin(getAngle());
+
     }
 
-    public double getAngle()
-    {
-        double angle = (double) Math.toDegrees(Math.atan2(yM-y, xM - x));
+    public double getAngle() {
+        double angle = Math.toDegrees(Math.atan2(yM - y, xM - x));
 
-        if(angle < 0){
+        if (angle < 0) {
             angle += 360;
         }
         return Math.toRadians(angle);
     }
 
-    public void move()
-    {
-        x+=dirX;
-        y+=dirY;
+    public double getX() {
+        return x;
     }
 
-    public void paint (Graphics2D g2d)
-    {
+    public double getY() { return y; }
+
+    public void move() {
+        x += dirX;
+        y += dirY;
+    }
+
+    public void paint(Graphics2D g2d) {
         g2d.setColor(Color.yellow);
-        g2d.fillRect(Math.round(this.x),Math.round(this.y),8,8);
+        g2d.fillRect((int)Math.round(this.x), (int)Math.round(this.y), 8, 8);
     }
 }
--- src/Level.java
+++ src/Level.java
@@ -9,29 +9,33 @@ import java.awt.event.MouseListener;
 
 public class Level extends JPanel {
     private Tank tank = new Tank(100, 100);
+    private Barrel barrel;
     private ArrayList<Bullet> playerB;
+    private static final int WIDTH = 1000;
+    private static final int HEIGHT = 700;
 
     public Level() {
         playerB = new ArrayList<Bullet>(0);
+        barrel = new Barrel(tank.getX(), tank.getY(),1,1);
 
         addMouseListener(new MouseListener() {
             @Override
-            public void mouseClicked(MouseEvent mouseEvent)  {
+            public void mouseClicked(MouseEvent mouseEvent) {
             }
 
             @Override
             public void mousePressed(MouseEvent e) {
-                Bullet b = new Bullet(tank.getX(),tank.getY(),e.getX(),e.getY());
+                Bullet b = new Bullet(tank.getX(), tank.getY(), e.getX(), e.getY());
                 playerB.add(b);
             }
 
             @Override
             public void mouseReleased(MouseEvent mouseEvent) {
-
             }
 
             @Override
             public void mouseEntered(MouseEvent mouseEvent) {
+
             }
 
             @Override
@@ -59,7 +63,19 @@ public class Level extends JPanel {
 
     //moving each non player element
     private void move() {
-        for (Bullet b : playerB)b.move();
+        for (Bullet b : playerB) b.move();
+    }
+
+    private void remove() {
+        ArrayList<Bullet> toRemove = new ArrayList<Bullet>(0);
+        if (!playerB.isEmpty()) {
+            for (Bullet b : playerB) {
+                if (b.getX() > WIDTH-5 || b.getX() < 5 || b.getY() > HEIGHT-5|| b.getY() < 5) {
+                    toRemove.add(b);
+                }
+            }
+            playerB.removeAll(toRemove);
+        }
     }
 
     @Override
@@ -67,8 +83,10 @@ public class Level extends JPanel {
         super.paint(g); //Clears the panel, for a fresh start
         Graphics2D g2d = (Graphics2D) g;
 
+
         tank.paint(g2d);
-        for (Bullet b : playerB)b.paint(g2d);
+        for (Bullet b : playerB) b.paint(g2d);
+        barrel.paint(g2d);
     }
 
 
@@ -76,13 +94,14 @@ public class Level extends JPanel {
         JFrame frame = new JFrame("CityScape");
         Level jp = new Level();
         frame.add(jp);
-        frame.setSize(1000, 700);
+        frame.setSize(WIDTH, HEIGHT);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         while (true) {
             jp.move(); //Updates the coordinates
             jp.repaint(); //Calls the paint method
+            jp.remove();
             Thread.sleep(1); //Pauses for a moment
         }
     }
