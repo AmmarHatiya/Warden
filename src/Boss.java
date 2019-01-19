4dcbd891f1b40677ccbc19d6c9f69017ab150133
Ammarhome <ammarhatiya>
2019-01-18 23:44:04 -0500

    Enemy images and movement

--- src/Boss.java
+++ src/Boss.java
@@ -11,6 +11,9 @@ public class Boss extends Entity {
     private static final int BOSSHIPRIGHT = 9;
     private static final int BOSSSHIPLEFT = 8;
     private int bossshipdirection = 9;
+    private static final int BOSSPLANERIGHT = 6;
+    private static final int BOSSPLANELEFT = 7;
+    private int bossplanedirection = 6;
     public int width;
     public int height;
     public int h;
@@ -18,22 +21,31 @@ public class Boss extends Entity {
     private int type;
     private int health;
     private int speed;
+    private final static Image bossplanerightimg;
 
-    private final static Image enemyturretimg;
-
-    private final static Image bossheliimg;
     static {
         try {
-            bossheliimg= ImageIO.read(Upgradesmenu.class.getResourceAsStream(""));
+            bossplanerightimg = ImageIO.read(Upgradesmenu.class.getResourceAsStream("bossplaneright.png"));
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
     }
-    private final static Image bossshipleftimg;
+
+    private final static Image bossplaneleftimg;
+
     static {
         try {
-            enemyturretimg = ImageIO.read(Upgradesmenu.class.getResourceAsStream("enemyturret.png"));
-            bossshipleftimg= ImageIO.read(Upgradesmenu.class.getResourceAsStream("bossshipleft.png"));
+            bossplaneleftimg = ImageIO.read(Upgradesmenu.class.getResourceAsStream("bossplaneleft.png"));
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private final static Image bossshipleftimg;
+
+    static {
+        try {
+            bossshipleftimg = ImageIO.read(Upgradesmenu.class.getResourceAsStream("bossshipleft.png"));
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
@@ -41,6 +53,7 @@ public class Boss extends Entity {
 
 
     private final static Image bossshiprightimg;
+
     static {
         try {
             bossshiprightimg = ImageIO.read(Upgradesmenu.class.getResourceAsStream("bossshipright.png"));
@@ -78,19 +91,49 @@ public class Boss extends Entity {
         if (type == BOSSTURRET) {
             width = 50;
             height = 50;
-            h=health = 25;
+            h = health = 25;
         } else if (type == BOSSSHIP) {
-            h=health = 100;
+            h = health = 100;
             width = 80;
             height = 30;
         } else if (type == BOSSTANK) {
             width = 50;
             height = 50;
-            h=health = 50;
+            h = health = 50;
         } else if (type == BOSSCOPTER) {
             width = 40;
             height = 30;
-            h=health = 75;
+            h = health = 75;
+        }
+    }
+
+    public void check(Entity p) {
+        if (x < p.x && x + width > p.x && y < p.y && y + height > p.y) {
+            this.x = x - vx;
+            this.y = y - vy;
+        }
+    }
+
+    public void shoot() {
+        int r1 = (int) (Math.random() * 1000 - 100);
+        int r2 = (int) (Math.random() * 1000 - 100);
+        this.addParticleToLevel.accept(new Bullet(this.x + 15, this.y + 15, r1, r2, false));
+    }
+
+    public void check(Particle p) {
+        if (p instanceof Bullet) {
+            Bullet b = (Bullet) p;
+            if (b.isPlayer && x < b.x && x + width > b.x && y < b.y && y + height > b.y) {
+                if (health > 0) {
+                    health--;
+                    this.removeParticleFromLevel.accept(p);
+                } else if (health <= 0) {
+                    this.addToScore.accept(h);
+                    this.removeParticleFromLevel.accept(p);
+                    this.removeSelf.accept(this);
+                }
+
+            }
         }
     }
 
@@ -102,8 +145,8 @@ public class Boss extends Entity {
             g2d.setColor(Color.red);
             g2d.fillRect(500 - health * 5, 5, health * 5, 25);
         }
-        Font txt = new Font("Monospaced", Font.BOLD, 15);
-        g2d.setColor(Color.black);
+        Font txt = new Font("Monospaced", Font.BOLD + Font.ITALIC, 16);
+        g2d.setColor(Color.WHITE);
         g2d.setFont(txt);
         g2d.drawString("Boss Health", 500, 25);
         //base
@@ -112,143 +155,87 @@ public class Boss extends Entity {
             g2d.drawImage(bossturretimg, (int) x, (int) y, 80, 95, null);
         } else if (type == BOSSTANK) {
             g2d.drawImage(bosstankimg, (int) x, (int) y, 80, 95, null);
-        } else if (type == BOSSSHIP && bossshipdirection == BOSSSHIPLEFT){
-                g2d.drawImage(bossshipleftimg,(int)x,(int)y,65,50,null);
-            }
-            else if (type == BOSSSHIP && bossshipdirection == BOSSHIPRIGHT){
-                g2d.drawImage(bossshiprightimg,(int)x,(int)y,65,50,null);
-            }
-            else if (type == BOSSCOPTER){
-                g2d.drawImage(bossheliimg,(int)x,(int) y,65,65,null);
+        } else if (type == BOSSSHIP && bossshipdirection == BOSSSHIPLEFT) {
+            g2d.drawImage(bossshipleftimg, (int) x, (int) y, 65, 50, null);
+        } else if (type == BOSSSHIP && bossshipdirection == BOSSHIPRIGHT) {
+            g2d.drawImage(bossshiprightimg, (int) x, (int) y, 65, 50, null);
+        } else if (type == BOSSCOPTER && bossplanedirection == BOSSPLANERIGHT) {
+            g2d.drawImage(bossplanerightimg, (int) x, (int) y, 65, 50, null);
+        } else if (type == BOSSCOPTER && bossplanedirection == BOSSPLANELEFT) {
+            g2d.drawImage(bossplaneleftimg, (int) x, (int) y, 65, 50, null);
         }
-
-        g2d.fillRect((int) x, (int) y, width, height);
-
-        g2d.setColor(Color.green);
-        g2d.fillOval((int) x, (int) y, 5, 5);
-
-
     }
 
-    public void tick(int levelWidth, int levelHeight) {
-        //vertical
-        // TURRET
-        int random = (int) (Math.random() * 100 + 1);
-        if (type == BOSSTURRET) {
-            if (random > 30) {
-            } else {
-                shoot();
-            }
-        } else if (type == BOSSTANK) {
-            if (random > 90) {
-                this.vy = speed;
-            } else if (random > 80) {
-                this.vy = -speed;
-            } else if (random > 10) {
-            // TANK vertical
-            if (type == BOSSTANK) {
+        public void tick ( int levelWidth, int levelHeight){
+            //vertical
+            // TURRET
+            int random = (int) (Math.random() * 100 + 1);
+            if (type == BOSSTURRET) {
+                if (random > 30) {
+                } else {
+                    shoot();
+                }
+            }// TANK vertical
+            else if (type == BOSSTANK) {
                 if (random > 95) {
                     this.vy = speed;
+                    shoot();
                 } else if (random > 90) {
                     this.vy = -speed;
-                } else if (random > 2) {
-
+                    shoot();
                 } else
                     shoot();
             }
             // TANK HORIZONTAL
-            random = (int) (Math.random() * 100 + 1);
-            if (type == 1) {
-                if (random > 95) {
+            if (type == BOSSTANK) {
+                if (random > 90) {
                     this.vx = speed;
-                } else if (random > 90) {
+                } else if (random > 80) {
                     this.vx = -speed;
                 } else if (random > 2) {
 
-            } else
-                shoot();
-            int r = (int) (Math.random() * 100 + 1);
-            if (r > 90) {
-                this.vx = speed;
-            } else if (r > 80) {
-                this.vx = -speed;
-            } else if (r > 2) {
+                } else
+                    shoot();
+            }
+// SHIP MOVEMENT (HORIZONTAL)
+            if (type == BOSSSHIP) {
+                if (random > 95) {
+                    bossshipdirection = BOSSHIPRIGHT;
+                    this.vx = speed;
+                } else if (random > 90) {
+                    bossshipdirection = BOSSSHIPLEFT;
+                    this.vx = -speed;
+                } else if (random > 2) {
 
-            } else
-                shoot();
+                } else
+                    shoot();
+            }
+            // HELICOPTER MOVEMENT
+            else if (type == BOSSCOPTER) {
+                if (random > 95) {
+                    bossplanedirection = BOSSPLANERIGHT;
+                    this.vx = speed;
+                } else if (random > 90) {
+                    bossplanedirection = BOSSPLANELEFT;
+                    this.vx = -speed ;
+                } else if (random > 20) {
                 } else
                     shoot();
             }
 
-// SHIP MOVEMENT (HORIZONTAL)
-        } else if (type == BOSSSHIP) {
-            if (random > 40) {
-            } else {
-            if (random > 95) {
-                this.vx = speed;
-                bossshipdirection = BOSSHIPRIGHT;
-            } else if (random > 90) {
-                bossshipdirection = BOSSSHIPLEFT;
-                this.vx = -speed;
-            } else if (random > 2) {
 
-            } else
-                shoot();
-            // HELICOPTER MOVEMENT
-        } else if (type == BOSSCOPTER) {
-            if (random > 90) {
-                this.vx = speed;
-            } else if (random > 80) {
-                this.vx = -speed;
-            } else if (random > 20) {
+            super.tick(levelWidth, levelHeight);
+            if (x > levelWidth - width || x < 5)
+                vx *= -speed;
+            if (y > levelHeight - 85 - height || y < 5)
+                vy *= -speed;
 
-            } else
-                shoot();
+
+            this.x = x + vx;
+            this.y = y + vy;
+
         }
 
 
-        super.tick(levelWidth, levelHeight);
-        if (x > levelWidth - width || x < 5)
-            vx *= -speed;
-        if (y > levelHeight - 85-height || y < 5)
-            vy *= -speed;
-
-
-        this.x = x + vx;
-        this.y = y + vy;
-
     }
 
-
-    public void shoot() {
-        int r1 = (int) (Math.random() * 1000 - 100);
-        int r2 = (int) (Math.random() * 1000 - 100);
-        this.addParticleToLevel.accept(new Bullet(this.x + 15, this.y + 15, r1, r2, false));
-    }
-
-    public void check(Entity p) {
-        if (x < p.x && x + width > p.x && y < p.y && y + height > p.y) {
-            this.x = x - vx;
-            this.y = y - vy;
-        }
-    }
-
-    public void check(Particle p) {
-        if (p instanceof Bullet) {
-            Bullet b = (Bullet) p;
-            if (b.isPlayer && x < b.x && x + width > b.x && y < b.y && y + height > b.y) {
-                if (health>0){
-                    health--;
-                    this.removeParticleFromLevel.accept(p);
-                }
-                else if (health<=0) {
-                    this.addToScore.accept(h);
-                    this.removeParticleFromLevel.accept(p);
-                    this.removeSelf.accept(this);
-                }
-
-            }
-        }
-    }
-
-}
